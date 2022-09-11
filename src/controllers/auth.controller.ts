48778cc6845d96bacf1c581486f9f998d7f9b008
leliovictor <leliov.vieira@gmail.com>
2022-09-10 22:12:59 -0300

    feat: added post /login feature

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,18 +3,17 @@ import { Request, Response } from "express";
 import * as service from "../services/auth.service.js";
 
 export async function postSignup(_req: Request, res: Response) {
-    const { body } = res.locals;
-    
-    await service.createUser(body);
+  const { body } = res.locals;
 
-    return res.sendStatus(201);
+  await service.createUser(body);
+
+  return res.sendStatus(201);
 }
 
-
 export async function postLogin(_req: Request, res: Response) {
-    //checa se email existe no banco de dados
-    //salva userId no jsow e retorna para usuario json
+  const { body } = res.locals;
 
+  const token = await service.loginUser(body);
 
-    return res.sendStatus(200);
+  return res.status(200).send(token);
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
-import bcrypt, { hash } from "bcrypt";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { Auth } from "../types/types.js";
 import { AppError } from "../middlewares/error.handler.middleware.js";
@@ -22,5 +23,54 @@ export async function createUser(body: Auth) {
 
   const passwordCript = bcrypt.hashSync(body.password, 10);
 
-  await repository.registerUser({email:body.email, password:passwordCript});
+  await repository.registerUser({ email: body.email, password: passwordCript });
+}
+
+export async function findUser(email: string) {
+  const user = await repository.findByEmail(email);
+
+  if (!user) {
+    throw new AppError(
+      401,
+      "E-mail or Password incorrect!",
+      "Ensure to provide a valid user/password"
+    );
+  }
+
+  return user;
+}
+
+export function comparePassword(
+  bodyPassword: string,
+  userPassword: string
+) {
+  const valid = bcrypt.compareSync(bodyPassword, userPassword);
+
+  if (!valid) {
+    throw new AppError(
+      401,
+      "E-mail or Password incorrect!",
+      "Ensure to provide a valid user/password"
+    );
+  }
+}
+
+function createToken(id: number) {
+    const TIME_60M = 60 * 60;
+  const secretKey = process.env.JWT_SECRET;
+  const data = { id };
+
+  const token = jwt.sign(data, secretKey, { expiresIn: TIME_60M });
+
+
+    return token;
+}
+
+export async function loginUser(body: Auth) {
+  const user = await findUser(body.email);
+  comparePassword(body.password, user.password);
+
+  const token = createToken(user.id);
+
+  return token;;
 }
